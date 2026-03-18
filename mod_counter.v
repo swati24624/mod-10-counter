26afae2033250a8a1deb41dd40bf311f4a686326
swati24624 <swati75372968@gmail.com>
2026-03-19 02:06:59 +0530

    Create mod_counter.v

---
+++ mod_counter.v
@@ -0,0 +1,16 @@
+module mod10_counter(
+    input clk,
+    input reset,
+    output reg [3:0] count
+);
+
+always @(posedge clk or posedge reset) begin
+    if (reset)
+        count <= 4'd0;
+    else if (count == 4'd9)
+        count <= 4'd0;
+    else
+        count <= count + 1;
+end
+
+endmodule
